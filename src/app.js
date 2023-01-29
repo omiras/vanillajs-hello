273215e29959090a4eea4730be7188b28d1b1d28
omiras <oscar.miras@gmail.com>
2023-01-29 06:34:26 +0000

    finished

--- src/app.js
+++ src/app.js
@@ -5,7 +5,27 @@ import "./style.css";
 import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
+let who = ["The dog", "My grandma", "His turtle", "My bird"];
+let action = ["ate", "peed", "crushed", "broke"];
+let what = ["my homework", "the keys", "the car"];
+let when = [
+  "before the class",
+  "right on time",
+  "when I finished",
+  "during my lunch",
+  "while I was praying"
+];
+
+function getRandomElement(array) {
+  return array[Math.floor(Math.random() * array.length)];
+}
+
 window.onload = function() {
-  //write your code here
-  document.writeln("<p>Test</p>");
+  let excuseWho = getRandomElement(who);
+  let excuseAction = getRandomElement(action);
+  let excuseWhat = getRandomElement(what);
+  let excuseWhen = getRandomElement(when);
+
+  document.querySelector("#excuse").innerHTML =
+    excuseWho + " " + excuseAction + " " + excuseWhat + " " + excuseWhen;
 };
